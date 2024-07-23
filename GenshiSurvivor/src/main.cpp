f44f11fe4a7ac94fde4b43fb5bcfcbc1f8cc6aba
qqssdda <sj1050437739@gmail.com>
2024-07-23 15:29:19 +0800

    创建背景与角色，角色移动代码开发

--- GenshiSurvivor/src/main.cpp
+++ GenshiSurvivor/src/main.cpp
@@ -1,4 +1,20 @@
 #include <easyx.h>
+#include <string>
+
+int idx_current_anim = 0;
+const int PLAYER_ANIM_NUM = 6;
+
+IMAGE img_player_left[PLAYER_ANIM_NUM];
+IMAGE img_player_right[PLAYER_ANIM_NUM];
+
+POINT player_pos = { 500,500 };
+const int PLAYER_SPEED = 2;
+
+#pragma comment(lib,"MSIMG32.LIB")
+
+inline void putimage_alptha(int x, int y, IMAGE* img);
+
+void LoadAnimation();
 
 int main()
 {
@@ -7,6 +23,15 @@ int main()
 	bool running = true;
 
 	ExMessage msg;
+	IMAGE img_background;
+
+	bool is_move_up = false;
+	bool is_move_down = false;
+	bool is_move_left = false;
+	bool is_move_right = false;
+
+	loadimage(&img_background, _T("./img/background.png"));
+	LoadAnimation();
 
 	BeginBatchDraw();
 	while (running)
@@ -15,10 +40,60 @@ int main()
 
 		while (peekmessage(&msg))
 		{
-
+			if (msg.message == WM_KEYDOWN)
+			{
+				switch (msg.vkcode)
+				{
+				case VK_UP:
+					is_move_up = true;
+					break;
+				case VK_DOWN:
+					is_move_down = true;
+					break;
+				case VK_LEFT:
+					is_move_left = true;
+					break;
+				case VK_RIGHT:
+					is_move_right = true;
+					break;
+				}
+			}
+			else if (msg.message == WM_KEYUP)
+			{
+				switch (msg.vkcode)
+				{
+				case VK_UP:
+					is_move_up = false;
+					break;
+				case VK_DOWN:
+					is_move_down = false;
+					break;
+				case VK_LEFT:
+					is_move_left = false;
+					break;
+				case VK_RIGHT:
+					is_move_right = false;
+					break;
+				}
+			}
 		}
 
+		if (is_move_up) player_pos.y -= PLAYER_SPEED;
+		if (is_move_down) player_pos.y += PLAYER_SPEED;
+		if (is_move_left) player_pos.x -= PLAYER_SPEED;
+		if (is_move_right) player_pos.x += PLAYER_SPEED;
+
+		static int counter = 0;
+		if (++counter % 5 == 0)
+			idx_current_anim++;
+
+		idx_current_anim = idx_current_anim % PLAYER_ANIM_NUM;
+
 		cleardevice();
+
+		putimage(0, 0, &img_background);
+		putimage_alptha(player_pos.x, player_pos.y, &img_player_left[idx_current_anim]);
+
 		FlushBatchDraw();
 
 		DWORD end_time = GetTickCount();
@@ -31,4 +106,27 @@ int main()
 	EndBatchDraw();
 	closegraph();
 	return 0;
+}
+
+inline void putimage_alptha(int x, int y, IMAGE* img)
+{
+	int w = img->getwidth();
+	int h = img->getheight();
+	AlphaBlend(GetImageHDC(NULL), x, y, w, h,
+		GetImageHDC(img),0,0,w,h,{AC_SRC_OVER,0,255,AC_SRC_ALPHA});
+}
+
+void LoadAnimation()
+{
+	for (int i = 0; i < PLAYER_ANIM_NUM; i++)
+	{
+		std::wstring path = L"./img/player_left_" + std::to_wstring(i) + L".png";
+		loadimage(&img_player_left[i], path.c_str());
+	}
+
+	for (int i = 0; i < PLAYER_ANIM_NUM; i++)
+	{
+		std::wstring path = L"./img/player_right_" + std::to_wstring(i) + L".png";
+		loadimage(&img_player_right[i], path.c_str());
+	}
 }
